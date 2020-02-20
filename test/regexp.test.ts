52cfd89aed4bd6ada94d9abd115dba7f18d44df8
whalemare <nanostet@gmail.com>
2020-02-20 15:15:02 +0700

    Add tests

--- test/regexp.test.ts
+++ test/regexp.test.ts
@@ -3,12 +3,20 @@ import { RegExpRule } from '../src/rule/RegExpRule'
 import chai from 'chai'
 
 describe(`regexp rule`, () => {
-  it('should return error message', () => {
+  it('should return passed error message', () => {
     const errorMessage = 'some random error text'
     const validator = new Validator()
     validator.push(new RegExpRule(/[0-9]/, errorMessage))
 
     const result = validator.validate('string')
     chai.assert.equal(result, errorMessage)
+  })
+
+  it('should return default error message', () => {
+    const validator = new Validator()
+    validator.push(new RegExpRule(/[0-9]/))
+
+    const result = validator.validate('string')
+    chai.assert.isString(result)
   })
 })
