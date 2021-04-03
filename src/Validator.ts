eba8156d986c223e443a630f9e2a34483f51649c
Anton Vlasov <nanostet@gmail.com>
2021-04-03 15:43:17 +0700

    Make Validator not extends array

--- src/Validator.ts
+++ src/Validator.ts
@@ -1,13 +1,13 @@
 import { Validatable } from './Validatable'
 
-export class Validator<T> extends Array<Validatable<T>> {
+export class Validator<T> {
   /**
    * Validate your input by rules
    * @param data input for validator
    * @returns null if error not exist, or error message
    */
   validate(data: T): string | null {
-    for (const rule of this) {
+    for (const rule of this.rules) {
       if (!rule.validate(data)) {
         const message = rule.errorMessage
         return message
@@ -16,4 +16,10 @@ export class Validator<T> extends Array<Validatable<T>> {
 
     return null
   }
+
+  push(rule: Validatable<T>) {
+    this.rules.push(rule)
+  }
+
+  constructor(private rules: Validatable<T>[] = []) {}
 }
